38ab6cac6b1b334371f08355d43d9521ac85e2f2
directx5 <73019195+directx5@users.noreply.github.com>
2021-08-05 16:14:14 +0300

    Chat option.

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class League:
         self.__session.auth = ('riot', self.__password)
         self.__session.verify = False
         disable_warnings(exceptions.InsecureRequestWarning)
+        self.summoner = self.request('get', '/lol-summoner/v1/current-summoner').json()
 
     def request(self, method, endpoint, data=None):
         return self.__session.request(method, urljoin(self.base_url, endpoint), data=dumps(data))
@@ -26,14 +27,15 @@ class League:
     def is_found(self):
         return self.request('get', '/lol-lobby/v2/lobby/matchmaking/search-state').json().get('searchState') == 'Found'
 
+    def is_searching(self):
+        value = self.request('get', '/lol-lobby/v2/lobby/matchmaking/search-state').json().get('searchState')
+        return value == 'Searching'
+
     def is_selecting(self):
         return self.request('get', '/lol-champ-select/v1/session').json().get('actions')
 
     def is_playing(self):
         return self.request('get', '/lol-gameflow/v1/session').json()['gameClient'].get('running')
-
-    def search(self):
-        self.request('post', '/lol-lobby/v2/lobby/matchmaking/search')
 
     def accept(self):
         self.request('post', '/lol-matchmaking/v1/ready-check/accept')
@@ -50,12 +52,39 @@ class League:
             me = 5 if len(x := [i for i in range(0, 5) if executor.submit(self.is_me, i).result()]) == 0 else x[0]
             self.select_champion(champion, me)
 
+    def chat(self, lane: str):
+        data = {'body': lane}
+        while True:
+            try:
+                chat_id = self.request('get', '/lol-chat/v1/conversations').json()[0]['id']
+
+                def sent_by(sid: int, cnt: str):
+                    messages = self.request('get', f'/lol-chat/v1/conversations/{chat_id}/messages').json()
+                    if any(x.get('fromSummonerId') == sid and cnt in x.get('body') for x in messages):
+                        return True
+                    else:
+                        return False
+
+                def everyone_in_lobby():
+                    session = self.request('get', '/lol-champ-select/v1/session').json()
+                    messages = self.request('get', f'/lol-chat/v1/conversations/{chat_id}/messages').json()
+                    return len(session['actions'][0]) == len(messages)
+
+                if everyone_in_lobby() and not sent_by(self.summoner['summonerId'], lane):
+                    self.request('post', f'/lol-chat/v1/conversations/{chat_id}/messages', data=data)
+                    break
+                else:
+                    continue
+            except IndexError:
+                continue
+
 
 if __name__ == '__main__':
-    client = League(r'*:\*\League of Legends')
+    client = League('League Path')
     while not client.is_playing():
         if client.is_selecting():
-            client.select("Champion")
+            client.select('Champion')
+            client.chat('Lane')
         elif client.is_found():
             client.accept()
         else:
