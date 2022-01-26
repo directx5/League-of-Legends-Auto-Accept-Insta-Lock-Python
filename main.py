432c298ae1f2cbab418af20a5cc42c945aed62f9
Saffat Hasan <saffathasan@gmail.com>
2022-01-25 23:22:04 -0500

    refactor: rewrite using sessions

--- main.py
+++ main.py
@@ -11,54 +11,56 @@ from urllib3 import disable_warnings, exceptions
 from constants.game_modes import GameModes
 
 
-class League:
-    def __init__(self, league: str):
-        with open('champions.txt', 'rb') as file:
-            self.champions = {(r := i.split(':'))[0]: r[1] for i in file.read().decode().splitlines()}
-        with open(path.join(league, 'lockfile'), 'r', encoding='UTF-8') as lockfile:
-            port, self.__password, protocol = lockfile.read().split(':')[2:]
+class LeagueAPI:
+    def __init__(self, league_path: str):
+        with open(path.join(league_path, 'lockfile'), 'r', encoding='UTF-8') as lockfile:
+            port, password, protocol = lockfile.read().split(':')[2:]
         self.base_url = f'{protocol}://127.0.0.1:{port}/'
         self.__session = Session()
-        self.__session.auth = ('riot', self.__password)
+        self.__session.auth = ('riot', password)
         self.__session.verify = False
         disable_warnings(exceptions.InsecureRequestWarning)
-        self.summoner = self.request('get', '/lol-summoner/v1/current-summoner').json()
 
     def request(self, method, endpoint, data=None):
         return self.__session.request(method, urljoin(self.base_url, endpoint), data=dumps(data))
 
-    def is_in_lobby(self):
-        return self.request('get', '/lol-lobby/v2/lobby').status_code == 200
-
-    def create_lobby(self, queueType):
-        return self.request('post', '/lol-lobby/v2/lobby', {"queueId": 450})
+    def create_lobby(self, queueId=450):
+        """ 
+            queueId 450 is ARAM
+            List of valid queue IDs are listed here: https://static.developer.riotgames.com/docs/lol/queues.json
+        """
+        self.request('post', '/lol-lobby/v2/lobby', {"queueId": queueId})
 
     def queue(self):
-        return self.request('post', '/lol-lobby/v2/lobby/matchmaking/search')
+        self.request('post', '/lol-lobby/v2/lobby/matchmaking/search')
 
-    def get_queue_state(self):
-        return self.request('get', '/lol-lobby/v2/lobby/matchmaking/search-state').json().get('searchState') 
-
-    def queue_pop(self):
-        return self.get_queue_state() == 'Found'
-
-    def is_searching(self):
-        return self.get_queue_state() == 'Searching' 
+    def session_phase(self):
+        return self.request('get', '/lol-gameflow/v1/session').json().get('phase')
 
     def accept(self):
-        return self.request('post', '/lol-matchmaking/v1/ready-check/accept')
+        self.request('post', '/lol-matchmaking/v1/ready-check/accept')
 
 if __name__ == '__main__':
-    client = League(config.LEAGUE_PATH)
+    client = LeagueAPI(config.LEAGUE_PATH)
+    print("Welcome to ARAM auto-queue.")
 
-    if not client.is_in_lobby():
-        client.create_lobby(GameModes.ARAM)
+    while True:
+        timeout = 5 # default polling period is 5 seconds
 
-    response = client.queue()
+        phase = client.session_phase()
+        if phase is None:
+            client.create_lobby()
+        elif phase == 'Lobby':
+            client.queue()
+        elif phase == 'Matchmaking':
+            print("Waiting for queue to pop")
+        elif phase == 'ReadyCheck':
+            client.accept()
+        elif phase in ['ChampSelect', 'InProgress', 'PreEndOfGame']:
+            # Waiting for game to complete, check once every 15 seconds
+            timeout = 15
+        elif phase == 'EndOfGame':
+            print("TODO: create lobby?")
 
-    while not client.queue_pop():
-        print("Waiting in queue...")
-        time.sleep(5)
-
-    client.accept()
+        time.sleep(timeout)
 
